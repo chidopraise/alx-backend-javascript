b3d5ef61649399f46c130de0c82ff9fc75b7fee5
Abraham Livinus <abrahamlivinus@gmail.com>
2024-07-31 16:24:58 -0700

    main.ts

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,36 +1,38 @@
-interface Teacher {
-  readonly firstName: string,
-  readonly lastName: string,
-  fullTimeEmployee: boolean,
-  yearsOfExperience?: number,
-  location: string,
-  [propName: string]: any,
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
 }
 
-interface Directors extends Teacher {
-  numberOfReports: number,
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+  
+class Director implements DirectorInterface {
+  workFromHome = () : string => 'Working from home';
+  getCoffeeBreak = () : string => 'Getting a coffee break';
+  workDirectorTasks = () : string => 'Getting to director tasks';
 }
 
-const printTeacher = (firstName:string, lastName:string) : string => `${firstName.charAt(0)}. ${lastName}`;
-
-class StudentClass {
-  firstName: string;
-  lastName: string;
-
-  constructor(firstName: string, lastName: string) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
-
-  workOnHomework = () : string => 'Currently working';
-
-  displayName = () : string => this.firstName;
+class Teacher implements TeacherInterface {
+  workFromHome = () : string => 'Cannot work from home';
+  getCoffeeBreak = () : string => 'Cannot have a break';
+  workTeacherTasks = () : string => 'Getting to work';
 }
 
-interface StudentConstructor {
-  (firstName: string, lastName: string): classInterface;
+export const createEmployee = (salary: number | string): Teacher | Director => Number(salary) < 500 ? new Teacher() : new Director();
+
+export const isDirector = (employee: Teacher | Director): boolean => employee instanceof Director;
+
+export const executeWork = (employee: Teacher | Director): void => {
+  const work = employee instanceof Teacher? employee.workTeacherTasks() : employee.workDirectorTasks();
+  console.log(work)
 }
 
-const student = new StudentClass("Paul", "jerry");
-console.log(student.displayName())
-console.log(student.workOnHomework())
+type Subjects = 'Math' | 'History';
+
+export const teachClass = (todayClass: Subjects) : string => {
+  return todayClass === 'Math' ? 'Teaching Math': 'Teaching History';
+}
